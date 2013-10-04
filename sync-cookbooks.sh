8082bac60724d8b91e7e5112fee82ff61d516121
Michael Dumont <michael.dumont@blackboard.com>
2013-10-04 16:32:05 -0400

    installing guest additions with chef

---
+++ sync-cookbooks.sh
@@ -0,0 +1,4 @@
+rsync -avz ~/p4/DIRE/projects/chef/chef-repo/cookbooks/vagrant_basebox ./cookbooks
+rsync -avz ~/p4/DIRE/projects/chef/chef-repo/cookbooks/bb_yum ./cookbooks
+rsync -avz ~/.berkshelf/cookbooks/sudo-fc9bd4aa1276313c680c0346618a49eaf55f9c11/ ./cookbooks/sudo
+rsync -avz ~/.berkshelf/cookbooks/yum-2.3.0/ ./cookbooks/yum
